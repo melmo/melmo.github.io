944db5ca6a9c225d2eb12f85747859468d40068d
Melanie Thewlis <melaniethewlis@littlewebgiants.com>
2015-07-12 17:22:48 +0200

    added tab index and wai aria pages to demo code for berlinjs access. talk

--- accessibility/berlin.js/code/app/scripts/main.js
+++ accessibility/berlin.js/code/app/scripts/main.js
@@ -1,2 +1,20 @@
 // jshint devel:true
-console.log('\'Allo \'Allo!');
+
+/*
+*
+*	Logical tab index 
+*
+*/
+
+var tabbable = ['a', 'input', 'select', 'button', 'textarea'];
+
+for (var i = 0; i < tabbable.length; i++) {
+	var elem = document.getElementsByTagName(tabbable[i]);
+	for (var j = 0; j < elem.length; j++) {
+		elem[j].setAttribute('tabindex', 0);
+	}
+}
+
+if (document.getElementById('main') !== null) {
+	document.getElementById('main').setAttribute('tabindex',0);
+}
