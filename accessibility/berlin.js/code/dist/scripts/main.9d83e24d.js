78ddfd6c2eff64978724851d3e60b27323b950ba
Melanie Thewlis <melaniethewlis@littlewebgiants.com>
2015-07-16 16:51:23 +0200

    do grunt build on new pages

--- accessibility/berlin.js/code/dist/scripts/main.9d83e24d.js
+++
@@ -1,1 +0,0 @@
-for(var tabbable=["a","input","select","button","textarea"],i=0;i<tabbable.length;i++)for(var elem=document.getElementsByTagName(tabbable[i]),j=0;j<elem.length;j++)elem[j].setAttribute("tabindex",0);null!==document.getElementById("main")&&document.getElementById("main").setAttribute("tabindex",0);
